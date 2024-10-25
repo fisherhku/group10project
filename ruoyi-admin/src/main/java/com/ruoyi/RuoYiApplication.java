7e353765cd3313e6bd69913b2dbe04f9fceace4d
fisherhku <168321203+fisherhku@users.noreply.github.com>
2024-10-25 10:24:38 +0800

    Initial commit

---
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -0,0 +1,52 @@
+package com.ruoyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ * 
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class RuoYiApplication
+{
+    public static void main(String[] args)
+    {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(RuoYiApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  GROUP 10 PROJECT Eshop SYSTEM STARTED SUCCESSFULLY   ლ(´ڡ`ლ)ﾞ  \n");
+        System.out.println("*********************************************");
+        System.out.println("*                                           *");
+        System.out.println("*      *******      *******      *******    *");
+        System.out.println("*     *       *    *       *    *       *   *");
+        System.out.println("*    *         *  *         *  *         *  *");
+        System.out.println("*   *           *            *            * *");
+        System.out.println("*  *             *            *            * *");
+        System.out.println("* *               *            *            * *");
+        System.out.println("* *               *            *            * *");
+        System.out.println("*  *             *            *            * *");
+        System.out.println("*   *           *            *            * *");
+        System.out.println("*    *         *  *         *  *         *  *");
+        System.out.println("*     *       *    *       *    *       *   *");
+        System.out.println("*      *******      *******      *******    *");
+        System.out.println("*                                           *");
+        System.out.println("*                 GROUP 10                  *");
+        System.out.println("*                                           *");
+        System.out.println("*      *******      *******      *******    *");
+        System.out.println("*     *       *    *       *    *       *   *");
+        System.out.println("*    *         *  *         *  *         *  *");
+        System.out.println("*   *           *            *            * *");
+        System.out.println("*  *             *            *            * *");
+        System.out.println("* *               *            *            * *");
+        System.out.println("* *               *            *            * *");
+        System.out.println("*  *             *            *            * *");
+        System.out.println("*   *           *            *            * *");
+        System.out.println("*    *         *  *         *  *         *  *");
+        System.out.println("*     *       *    *       *    *       *   *");
+        System.out.println("*      *******      *******      *******    *");
+        System.out.println("*                                           *");
+        System.out.println("*********************************************");
+    }
+}
